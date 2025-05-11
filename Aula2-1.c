a43534048ab2f0d74588d454df0104e16c48475c
MatheusNobreM <matheusipsnobre@gmail.com>
2025-05-11 16:42:43 -0300

    até a aula 3.1

---
+++ Aula2-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Valor inteiro: %d.\n", 10);
+    printf("Valor real: %f.\n", 10.597976);
+    printf("Valor real com 2 casas decimais: %.2f.\n", 10.5987);
+    printf("Dado texto : %c.\n", 'A');
+    printf("Dado texto : %s.\n", "Aula 2 - 1");
+}
---
+++ Aula2-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 0;
+    
+    printf("Digite a sua idade: %d.\n", idade);
+    printf("Digite a sua idade: \n");
+    scanf("%d", &idade);
+    printf("A sua idade é: %d.\n", idade);
+    return 0;
+}
